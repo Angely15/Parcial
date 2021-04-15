c1bf55e7d0856df8c609d53d209bb022e9a2d061
angieandreabayonavillabona <angelybayona15@gmail.com>
2021-04-15 09:04:08 -0400

    sexto commit

--- script.js
+++ script.js
@@ -2,15 +2,14 @@ window.addEventListener("load", function () {
     fetch("http://demo6497253.mockable.io/noticias/")
       .then((response) => response.json())
       .then((data) => {
-        document.getElementById("contenido").innerHTML = data
-          .map(
+        document.getElementById("contenido").innerHTML = data.map(
             (e) => `
             <h3>${e.titulo}</h3>
             <p class="border">
               ${e.contenido}
               <a href="http://">Ver mas.</a>
             </p>
-     `
+     ` 
           )
           .join("");
       });
